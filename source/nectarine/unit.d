ac3e09a7578e7eb82dd23c1f891eeab7b49a964e
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-02-10 22:09:31 -0500

    Update unit.d

--- source/nectarine/unit.d
+++ source/nectarine/unit.d
@@ -14,17 +14,21 @@ class Unit(Space, Damage, Percentage) : NectarineObject!Space if (isNumeric!(Dam
 {
     private
     {
-        alias PUT = Percentage[UnitType];
+        alias UT = UnitType;
+        alias PUT = Percentage[UT];
         
+        UT type;
         PUT attackPowerVariations;
         Space movementRange, attackRange;
         Damage healthPoints, attackPower;
         bool hasMoved, hasAttacked, canAttackAfterMoving;
     }
     
-    this(Space x, Space y, Space mr, Damage hp, Damage ap, Space ar = 1, bool caam = true, PUT apv = null)
+    this(Space x, Space y, UT ut, Space mr, Damage hp, Damage ap, Space ar = 1, bool caam = true, PUT apv = null)
     {
         super(x, y);
+        
+        type = ut;
         
         attackRange = ar;
         attackPower = ap;
@@ -40,24 +44,38 @@ class Unit(Space, Damage, Percentage) : NectarineObject!Space if (isNumeric!(Dam
         return healthPoints;
     }
     
+    auto getType()
+    {
+        return type;
+    }
+    
     auto getDamageFor(Unit unit, Tile occupied)
     {
-        defense = occupied.getDefenseBonus;
+        auto defense = unit.getHealthPoints / occupied.getDefenseBonus * 100;
+        Damage attack;
+        
+        if (unit.getType !in attackPowerVariations)
+            attack = attackPower / healthPoints * 100;
+            
+        else
+            attack = (attackPower / attackPowerVariations * 100) / healthPoints * 100;
+        
+        return attack - defense;
     }
 }
 
 class Unit(T, G) : Unit!(T, G, G)
 {
-    this(T x, T y, T mr, G hp, G ap, T ar = 1, bool caam = true, G[UnitType] apv = null)
+    this(T x, T y, UnitType ut, T mr, G hp, G ap, T ar = 1, bool caam = true, G[UnitType] apv = null)
     {
-        super(x, y, mr, hp, ap, ar, caam, apv);
+        super(x, y, ut, mr, hp, ap, ar, caam, apv);
     }
 }
 
 class Unit(T) : Unit!(T, T, T)
 {
-    this(T x, T y, T mr, T hp, T ap, T ar = 1, bool caam = true, T[UnitType] apv = null)
+    this(T x, T y, UnitType ut, T mr, T hp, T ap, T ar = 1, bool caam = true, T[UnitType] apv = null)
     {
-        super(x, y, mr, hp, ap, ar, caam, apv);
+        super(x, y, ut, mr, hp, ap, ar, caam, apv);
     }
 }
