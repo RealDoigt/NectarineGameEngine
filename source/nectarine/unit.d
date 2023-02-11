65a7c321c3b67f425a282aebb328e6a56d317354
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2023-02-10 21:13:44 -0500

    Update unit.d

--- source/nectarine/unit.d
+++ source/nectarine/unit.d
@@ -1,4 +1,5 @@
 module nectarine.unit;
+import std.traits;
 
 enum UnitType
 {
@@ -6,4 +7,36 @@ enum UnitType
     cavalry,
     naval,
     air
+}
+
+class Unit(Space, Damage, Percentage) : NectarineObject!Space if (isNumeric!(Damage) && isNumeric(Percentage))
+{
+    private
+    {
+        Space movementRange, attackRange;
+        Damage healthPoints, attackPower;
+        Percentage[UnitType] attackPowerVariation;
+        bool hasMoved, hasAttacked, canAttackAfterMoving;
+    }
+    
+    this(Space x, Space y)
+    {
+        super(x, y);
+    }
+}
+
+class Unit(T, G) : Unit!(T, G, G)
+{
+    this(T x, T y)
+    {
+        super(x, y);
+    }
+}
+
+class Unit(T) : Unit!(T, T, T)
+{
+    this(T x, T y)
+    {
+        super(x, y);
+    }
 }
